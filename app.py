c5b640a995af1a000fb06b8d3c435df412fbdbca
sumaa0219 <sumaa0219@gmail.com>
2022-09-25 01:07:12 -0400

    aaa

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import room
 BaseBackColor = "black"
 
 root = tk.Tk()
-root.geometry("1024x600")
+root.geometry("1265x690")
 menubar = tk.Menu(root)
 root.configure(menu = menubar,bg=BaseBackColor)
 filemenu = tk.Menu(menubar, tearoff = 0)
@@ -23,19 +23,19 @@ CL.calender(calender,25,BaseBackColor)
 calender.place(x=35,y=15)
 
 time = tk.LabelFrame(root,width=445, height=150,bg=BaseBackColor,bd=0)
-DC.Time(time,175,BaseBackColor)
-time.place(x=470,y=40)
+DC.Time(time,140,BaseBackColor)
+time.place(x=550,y=40)
 
 date = tk.LabelFrame(root,width=330, height=65,bg=BaseBackColor,bd=0)
 DC.Date(date,54,BaseBackColor)
-date.place(x=650,y=240)
+date.place(x=670,y=240)
 
 second= tk.LabelFrame(root,width=75, height=60,bg=BaseBackColor,bd=0)
 DC.Second(second,64,BaseBackColor)
-second.place(x=900,y=150)
+second.place(x=1100,y=150)
 
-temp= tk.LabelFrame(root,width=230, height=105,bg=BaseBackColor,bd=0)
-weather.temp(temp,111,BaseBackColor)
+temp= tk.LabelFrame(root,width=350, height=105,bg=BaseBackColor,bd=0)
+weather.temp(temp,100,BaseBackColor)
 temp.place(x=530,y=465)
 
 place= tk.LabelFrame(root,width=450, height=40,bg=BaseBackColor,bd=0)
@@ -48,23 +48,23 @@ condition.place(x=760,y=400)
 
 maxtemp= tk.LabelFrame(root,width=150, height=40,bg=BaseBackColor,bd=0)
 weather.MaxTemp(maxtemp,35,BaseBackColor)
-maxtemp.place(x=825,y=465)
+maxtemp.place(x=900,y=460)
 
 lowtemp= tk.LabelFrame(root,width=150, height=40,bg=BaseBackColor,bd=0)
 weather.LowTemp(lowtemp,35,BaseBackColor)
-lowtemp.place(x=825,y=510)
+lowtemp.place(x=900,y=510)
 
 humidity= tk.LabelFrame(root,width=150, height=40,bg=BaseBackColor,bd=0)
 weather.humidity(humidity,35,BaseBackColor)
-humidity.place(x=825,y=550)
+humidity.place(x=900,y=560)
 
 RoTemp= tk.LabelFrame(root,width=200, height=45,bg=BaseBackColor,bd=0)
 room.Temp(RoTemp,43,BaseBackColor)
-RoTemp.place(x=105,y=460)
+RoTemp.place(x=105,y=470)
 
 RoHum= tk.LabelFrame(root,width=200, height=45,bg=BaseBackColor,bd=0)
 room.Hum(RoHum,43,BaseBackColor)
-RoHum.place(x=105,y=510)
+RoHum.place(x=105,y=530)
 
 
 wea = weather.reload()
